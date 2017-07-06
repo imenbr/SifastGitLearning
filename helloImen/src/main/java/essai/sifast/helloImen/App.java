9a194aaaedf5788cc4da3ec8f48db4048c6b71ed
jiheneA <jihenee.abdelhedi@gmail.com>
2017-07-06 17:12:19 +0100

    hello imen

---
+++ helloImen/src/main/java/essai/sifast/helloImen/App.java
@@ -0,0 +1,13 @@
+package essai.sifast.helloImen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
